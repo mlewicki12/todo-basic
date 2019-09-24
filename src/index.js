d6e76d5bbec56bd67c209015526f72b1d9958781
Maciej Lewicki <lewickima@drexel.edu>
2019-09-24 10:52:54 -0400

    hiding checked items works

--- src/index.js
+++ src/index.js
@@ -6,30 +6,37 @@ import './index.css'
 class TodoApp extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { items: [], text: "" };
+        this.state = { items: [], text: "", hideChecked: false };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.handleChangeChecked = this.handleChangeChecked.bind(this);
     }
 
     render() {
         return (
             <div>
-                <h3>TODO LIST</h3>
-                <TodoList items={this.state.items} />
-                <form onSubmit={this.handleSubmit}>
-                    <label htmlFor="new-todo">
-                        Add a task
-                    </label><br />
-                    <input
-                        id="new-todo"
-                        onChange={this.handleChange}
-                        value={this.state.text}
-                    />
-                    <button>
-                        Add #{this.state.items.length + 1}
-                    </button>
-                </form>
+                <div>
+                    <h3>TODO LIST</h3>
+                    <TodoList items={this.state.items} hideChecked={this.state.hideChecked} />
+                    <form onSubmit={this.handleSubmit}>
+                        <label htmlFor="new-todo">
+                            Add a task
+                        </label><br />
+                        <input
+                            id="new-todo"
+                            onChange={this.handleChange}
+                            value={this.state.text}
+                        />
+                        <button>
+                            Add
+                        </button>
+                    </form>
+                </div>
+                <div>
+                    <TodoOptions onChange={this.handleChangeChecked} display={true}/>
+                </div>
             </div>
         );
     }
@@ -54,6 +61,12 @@ class TodoApp extends React.Component {
             text: ""
         }))
     }
+
+    handleChangeChecked(e) {
+        this.setState({
+            hideChecked: !this.state.hideChecked
+        })
+    }
 }
 
 class TodoCheck extends React.Component {
@@ -64,20 +77,30 @@ class TodoCheck extends React.Component {
             checked: false
         };
 
+        if(this.props.onChange) {
+            this.handleChange = this.props.onChange;
+        }
+
         this.handleChange = this.handleChange.bind(this);
     }
 
     render() {
-        let status = this.state.checked ? "checkbox slash" : "checkbox";
+        let status = (this.state.checked && this.props.slash) ? "checkbox slash" : "checkbox";
 
-        return (
-                <li>
-                    <label className={status}>
-                        <input type="checkbox" onChange={this.handleChange}></input>
-                        {this.props.text}
-                    </label>
-                </li>
-        )
+        if(!this.props.hide || !this.state.checked) {
+            return (
+                    <li>
+                        <label className={status}>
+                            <input
+                                type="checkbox"
+                                onChange={this.handleChange}
+                                checked={this.state.checked || this.props.display}>
+                            </input>
+                            {this.props.text}
+                        </label>
+                    </li>
+            )
+        } else return null;
     }
 
     handleChange(e) {
@@ -91,10 +114,32 @@ class TodoList extends React.Component {
     render() {
         return (
             <ul>
-                {this.props.items.map(item => 
-                    <TodoCheck key={item.id} onChange={this.onChange} text={item.text} />
+                {this.props.items.map(item =>
+                    <TodoCheck key={item.id} slash={true} hide={this.props.hideChecked} text={item.text} />
                 )}
             </ul>
+        )
+    }
+}
+
+class TodoOptions extends React.Component {
+    render() {
+        return (
+            <div>
+                <h4>OPTIONS</h4>
+                <ul>
+                    <TodoCheck
+                        key="hideChecked"
+                        slash={false}
+                        hide={false}
+                        onChange={this.props.onChange} 
+                        // this requires the bind
+                        // I assume this is because otherwise the function is passed and
+                        // it's not part of the class anymore, because this is undefined
+                        text="Hide Checked Items"
+                    />
+                </ul>
+            </div>
         )
     }
 }
